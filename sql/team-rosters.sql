c99b674ec4c08f2969d24b9facda9da93d85c374
Dan Cusick <dan.cusick@gmail.com>
2021-08-01 12:57:34 -0500

    Home team roster queries MAN vs LIV joined to player_attributes - removed nulls.

--- sql/team-rosters.sql
+++ sql/team-rosters.sql
@@ -1,8 +1,12 @@
 #
-# Queries to pull the home team rosters for MAN (10260) vs LIV(8650)
+# Queries to pull the home team rosters for all MAN (10260) vs LIV(8650) games
+# and join on player_attributes
 #
 
-select distinct p.player_name, p.player_api_id, m.home_team_api_id from player p, match m 
+drop table if exists livvman;
+select distinct p.player_name, p.player_api_id, m.home_team_api_id 
+      into temporary table livvman
+      from player p, match m 
       where ((p.player_api_id = m.home_player_1) 
 	  or (p.player_api_id = m.home_player_2)
 	  or (p.player_api_id = m.home_player_3)
@@ -14,9 +18,15 @@ select distinct p.player_name, p.player_api_id, m.home_team_api_id from player p
 	  or (p.player_api_id = m.home_player_9)
 	  or (p.player_api_id = m.home_player_10)
 	  or (p.player_api_id = m.home_player_11))
-	  and ((m.home_team_api_id = 8650) and (away_team_api_id = 10260))
+	  and ((m.home_team_api_id = 8650) and (away_team_api_id = 10260));
 
-select distinct p.player_name, p.player_api_id, m.home_team_api_id from player p, match m 
+select * from livvman 
+left join player_attributes on livvman.player_api_id = player_attributes.player_api_id;
+
+drop table if exists manvliv;
+select distinct p.player_name, p.player_api_id, m.home_team_api_id 
+      into temporary table manvliv
+      from player p, match m 
       where ((p.player_api_id = m.home_player_1) 
 	  or (p.player_api_id = m.home_player_2)
 	  or (p.player_api_id = m.home_player_3)
@@ -28,4 +38,7 @@ select distinct p.player_name, p.player_api_id, m.home_team_api_id from player p
 	  or (p.player_api_id = m.home_player_9)
 	  or (p.player_api_id = m.home_player_10)
 	  or (p.player_api_id = m.home_player_11))
-	  and ((m.home_team_api_id = 10260) and (away_team_api_id = 8650))
+	  and ((m.home_team_api_id = 10260) and (away_team_api_id = 8650));
+
+select * from manvliv 
+left join player_attributes on manvliv.player_api_id = player_attributes.player_api_id;
