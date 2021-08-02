fa442d2e10c428297b9aab917a01eb66c91a0775
Dan Cusick <dan.cusick@gmail.com>
2021-08-01 20:31:18 -0500

    Updated the team roster queries to include all combinations of Mun@LIv and Liv@Mun. Joined to the team_attributes table.

--- sql/team-rosters.sql
+++ sql/team-rosters.sql
@@ -1,7 +1,12 @@
-#
-# Queries to pull the home team rosters for all MAN (10260) vs LIV(8650) games
-# and join on player_attributes
-#
+--#
+--# Queries to pull the team rosters for all MAN (10260) vs LIV(8650) games
+--# and join on player_attributes
+--#
+
+--#
+--# Select home player roster for when MUN played at Liverpool
+--#
+--#
 
 drop table if exists livvman;
 select distinct p.player_name, p.player_api_id, m.home_team_api_id 
@@ -23,6 +28,37 @@ select distinct p.player_name, p.player_api_id, m.home_team_api_id
 select * from livvman 
 left join player_attributes on livvman.player_api_id = player_attributes.player_api_id;
 
+--#
+--# Select away player roster for when MUN played at Liverpool
+--#
+--#
+
+drop table if exists manvliv;
+select distinct p.player_name, p.player_api_id, m.away_team_api_id 
+      into temporary table manvliv
+      from player p, match m 
+      where ((p.player_api_id = m.away_player_1) 
+	  or (p.player_api_id = m.away_player_2)
+	  or (p.player_api_id = m.away_player_3)
+	  or (p.player_api_id = m.away_player_4)
+	  or (p.player_api_id = m.away_player_5)
+	  or (p.player_api_id = m.away_player_6)
+	  or (p.player_api_id = m.away_player_7)
+	  or (p.player_api_id = m.away_player_8)
+	  or (p.player_api_id = m.away_player_9)
+	  or (p.player_api_id = m.away_player_10)
+	  or (p.player_api_id = m.away_player_11))
+	  and ((m.away_team_api_id = 10260) and (home_team_api_id = 8650));	
+
+select * from manvliv
+left join player_attributes on manvliv.player_api_id = player_attributes.player_api_id;
+
+
+--#
+--# Select home player roster for when Liverpool played at MUN
+--#
+--#
+
 drop table if exists manvliv;
 select distinct p.player_name, p.player_api_id, m.home_team_api_id 
       into temporary table manvliv
@@ -42,3 +78,29 @@ select distinct p.player_name, p.player_api_id, m.home_team_api_id
 
 select * from manvliv 
 left join player_attributes on manvliv.player_api_id = player_attributes.player_api_id;
+
+--#
+--# Select away player roster for when Liverpool played at MUN
+--#
+--#
+
+drop table if exists livvman;
+select distinct p.player_name, p.player_api_id, m.away_team_api_id 
+      into temporary table livvman
+      from player p, match m 
+      where ((p.player_api_id = m.away_player_1) 
+	  or (p.player_api_id = m.away_player_2)
+	  or (p.player_api_id = m.away_player_3)
+	  or (p.player_api_id = m.away_player_4)
+	  or (p.player_api_id = m.away_player_5)
+	  or (p.player_api_id = m.away_player_6)
+	  or (p.player_api_id = m.away_player_7)
+	  or (p.player_api_id = m.away_player_8)
+	  or (p.player_api_id = m.away_player_9)
+	  or (p.player_api_id = m.away_player_10)
+	  or (p.player_api_id = m.away_player_11))
+	  and ((m.away_team_api_id = 8650) and (home_team_api_id = 10260));	
+
+select * from livvman
+left join player_attributes on livvman.player_api_id = player_attributes.player_api_id;
+
